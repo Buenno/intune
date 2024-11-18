d6a28c77483a2da3b7d537e9224ef5e438a45eec
Toby Williams <git@iambueno.co.uk>
2024-11-18 10:31:31 +0000

    Updated install and uninstall templates

--- App Install/# Templates/Install/EXE_Install-Application.ps1
+++ App Install/# Templates/Install/EXE_Install-Application.ps1
@@ -1,11 +1,18 @@
 $ErrorActionPreference = 'Stop'
 
+$appName = ""
 $binary = Get-ChildItem -Path "$PSScriptRoot\binary" -Filter *.exe
 $installer = $binary.FullName
+$installOp = "Installation"
 
 $installParams = @(
     "/silent"
 )
 
 # Install application
-Start-Process $installer -ArgumentList "$($installParams -join " ")" -PassThru -Wait
+$i = Start-Process $installer -ArgumentList "$($installParams -join " ")" -PassThru -Wait
+
+# Add status registry key
+if ($i.ExitCode -eq 0){
+    Add-StatusRegistryProperty -Application $appName -Operation $installOp -Status '0' 
+}
