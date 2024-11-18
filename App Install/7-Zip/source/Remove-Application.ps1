7b33238ee1ceff3cceb84e53ec39bc2a059e5076
Toby Williams <git@iambueno.co.uk>
2024-11-18 14:06:36 +0000

    Updated 7-Zip to use templates

---
+++ App Install/7-Zip/source/Remove-Application.ps1
@@ -0,0 +1,54 @@
+$ErrorActionPreference = 'Stop'
+
+function Remove-StatusRegistryKey {
+    <#
+    .SYNOPSIS
+    Removes an application status key from the registry.
+
+    .DESCRIPTION
+    Removes an application installation status key from the registry based on the supplied application name.
+
+    .PARAMETER Application
+    The name of the application as listed in the registry.
+    
+    .OUTPUTS
+    None
+
+    .EXAMPLE
+    Remove-StatusRegistryKey -Application "Google Chrome"
+    #>
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory = $true)]
+        [string[]] $Application
+    )
+    BEGIN {
+        $ErrorActionPreference = [System.Management.Automation.ActionPreference]::Stop
+    }
+    PROCESS {
+        $statusReg = "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Intune_Win32\$Application"
+        if ((Test-Path $statusReg)){
+            Remove-Item -Path $statusReg -Recurse -Force
+        }
+    }
+} 
+
+$appName = "7-Zip"
+$installer = "msiexec.exe"
+
+# Get uninstall string from registry
+$uninstallReg = Get-ChildItem -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall | Get-ItemProperty | Where-Object {$_.DisplayName -like "$appName*"}
+
+$uninstallParams = @(
+    "/x",
+    $uninstallReg.PSChildName,
+    "/qn"
+)
+
+# Uninstall application
+$u = Start-Process $installer -ArgumentList "$($uninstallParams -join " ")" -PassThru -Wait
+
+# Remove status registry key
+if ($u.ExitCode -eq 0){
+    Remove-StatusRegistryKey -Application $appName 
+}
