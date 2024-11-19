95d7362dcc6834e249cf9202e5437b7be77ee764
Toby Williams <git@iambueno.co.uk>
2024-11-19 10:45:31 +0000

    Updated Google Drive to use templates

--- App Install/Drive for Desktop/source/Remove-Application.ps1
+++ App Install/Drive for Desktop/source/Remove-Application.ps1
@@ -1,30 +1,61 @@
 $ErrorActionPreference = 'Stop'
 
-## Get the uninstall string
-$application = "Google Drive"
-$uReg = (Get-ChildItem -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall | Get-ItemProperty | Where-Object {$_.DisplayName -match $($application)})
-$uParams = @(
+function Remove-StatusRegistryKey {
+    <#
+    .SYNOPSIS
+    Removes an application status key from the registry.
+
+    .DESCRIPTION
+    Removes an application installation status key from the registry based on the supplied application name.
+
+    .PARAMETER Application
+    The name of the application as listed in the registry.
+    
+    .OUTPUTS
+    None
+
+    .EXAMPLE
+    Remove-StatusRegistryKey -Application "Google Chrome"
+    #>
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory = $true)]
+        [string[]] $Application
+    )
+    BEGIN {
+        $ErrorActionPreference = [System.Management.Automation.ActionPreference]::Stop
+    }
+    PROCESS {
+        $statusReg = "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Intune_Win32\$Application"
+        if ((Test-Path $statusReg)){
+            Remove-Item -Path $statusReg -Recurse -Force
+        }
+    }
+} 
+
+# Get the uninstall string from registry
+$appName = "Google Drive"
+$uninstallReg = Get-ChildItem -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall | Get-ItemProperty | Where-Object {$_.DisplayName -eq $appName}
+$configPath =   "HKLM:\SOFTWARE\Policies\Google\DriveFS"
+
+$uninstallParams = @(
     "--silent",
     "--force_stop"
-    )
+)
 
-## Uninstall application
+# Uninstall application
+$u = Start-Process $uninstallReg.UninstallString -ArgumentList $uninstallParams -PassThru -Wait
+
+# Delete configuration
 try {
-    Start-Process "$($uReg.UninstallString)" -ArgumentList "$($uParams -join " ")" -Wait
+    Remove-Item $configPath -Force
+    $configDel = $true
 }
 catch {
-    Write-Host "Uninstall failed. The following error was returned: $($_.Exception.Message)"
+    $configDel = $false
 }
 
-## Delete registry keys
-$DriveReg = "HKLM:\SOFTWARE\Policies\Google\DriveFS"
-
-if ($LASTEXITCODE -eq 0){
-    try {
-        Remove-Item $DriveReg -Force
-        Remove-Item $uReg.PSPath -Force
-    }
-    catch {
-        Write-Host "Unable to remove registry - $($_.Exception.Message)"
-    }
+# Remove status registry key
+if (($u.ExitCode -eq 0) -and ($configDel)){
+    Remove-StatusRegistryKey -Application $appName 
 }
