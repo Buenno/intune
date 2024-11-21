5d97bdfe55633ef4591f00630c2be25775791eed
Toby Williams <git@iambueno.co.uk>
2024-11-21 12:54:17 +0000

    Added Papercut Print Deploy Client

---
+++ App Install/Papercut Print Deploy/detection/PapercutPDDetect.ps1
@@ -0,0 +1,23 @@
+# Check if the uninstall key exists
+$appName = "Papercut Print Deploy"
+$uninstallReg = Get-ChildItem -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall" | Get-ItemProperty | Where-Object {$_.DisplayName -like "$appName*"} -ErrorAction SilentlyContinue
+$statusReg =    "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Intune_Win32\$appName"
+
+# Check if status registry keys exist
+$installOp =    "Installation"
+
+if (Test-Path -Path $statusReg){
+    $sRegCheck = Get-ItemPropertyValue -Path $statusReg -Name $installOp | Where-Object {$_ -eq "0"}
+}
+else {
+    $sRegCheck = $false
+}
+
+if (($sRegCheck) -and ($uninstallReg)){
+    Write-Host "$appName is installed"
+    exit 0
+}
+else {
+    Write-Host "$appName is not installed"
+    exit 1
+}
